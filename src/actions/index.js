8b59bec2f6d854f4a62625c24046af4177fbe5e0
umang-jain <umang.usict.042164@ipu.ac.in>
2019-03-23 01:30:49 +0530

    use lodash action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,9 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => {
   return async (dispatch,getState) => {
     await dispatch(fetchPosts());
-    getState().posts
+    const userIds = _.uniq(_.map(getState().posts,'userId'))
+    userIds.forEach((id) => dispatch(fetchUser(id)));
+
   }
 }
 
